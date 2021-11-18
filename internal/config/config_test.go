f215a892f51bb450be0eced67b5c61df6a7af295
Ricardo Hernandez <ricardo.hernandez@mongodb.com>
2021-11-18 16:17:18 +0000

    Add test for config

---
+++ internal/config/config_test.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// UserGroupsInConfig matches the user groups (from claims) that are
+// present in config and returns a filtered set of Groups
+func TestValidUserGroups(t *testing.T) {
+
+	userGroups := []string{"one", "two", "three"}
+
+	groups := map[string]Group{
+		"two": Group{
+			Orgs: []Org{
+				{
+					ID:   1,
+					Role: "Editor",
+				},
+			},
+		},
+	}
+
+	validGroups := ValidUserGroups(userGroups, groups)
+	assert.Contains(t, validGroups, "two")
+	assert.NotContains(t, validGroups, "one")
+}
