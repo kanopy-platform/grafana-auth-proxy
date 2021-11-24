8be7240cf0e750c6db6e0b21fe94f5e83f0d0ab6
Ricardo Hernandez <ricardo.hernandez@mongodb.com>
2021-11-24 19:47:57 +0000

    revamp grafana client testing

--- pkg/grafana/client_test.go
+++ pkg/grafana/client_test.go
@@ -5,6 +5,7 @@ import (
 
 	gapi "github.com/grafana/grafana-api-golang-client"
 	"github.com/grafana/grafana/pkg/models"
+	"github.com/kanopy-platform/grafana-auth-proxy/internal/config"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -39,7 +40,7 @@ func TestCreateUser(t *testing.T) {
 func TestAddOrgUser(t *testing.T) {
 	user := setupUser()
 
-	orgRoleMap := map[int64]models.RoleType{
+	orgRoleMap := userOrgsRoleMap{
 		1: models.ROLE_EDITOR,
 	}
 	client := NewMockClient(user, orgRoleMap)
@@ -56,15 +57,88 @@ func TestAddOrgUser(t *testing.T) {
 func TestUpsertOrgUser(t *testing.T) {
 	user := setupUser()
 
-	orgRoleMap := map[int64]models.RoleType{
+	orgRoleMap := userOrgsRoleMap{
 		1: models.ROLE_EDITOR,
 	}
 
 	client := NewMockClient(user, orgRoleMap)
 
-	// this should always succedd except for error when calling the rest api
+	// this should always succeed except for errors when calling the rest api
 	err := client.UpsertOrgUser(1, user, "Editor")
 	assert.Nil(t, err)
+
+	// upsert will return an error when the orgID is invalid for example
+	err = client.UpsertOrgUser(0, user, "Admin")
+	assert.NotNil(t, err)
+
+	// if user doesn't exists then Upsert will return an error on update user path
+	err = client.UpsertOrgUser(1, gapi.User{}, "Viewer")
+	assert.NotNil(t, err)
+}
+
+// This is a silly test as the mock always returns nil but it's here for completeness
+func TestUpdateUserPermissions(t *testing.T) {
+	user := setupUser()
+
+	client := NewMockClient(user, userOrgsRoleMap{})
+
+	err := client.UpdateUserPermissions(user.ID, true)
+	assert.NoError(t, err)
+}
+
+func TestUpdateOrgUserAuthz(t *testing.T) {
+
+	user := setupUser()
+
+	groups := config.Groups{
+		"foo": {
+			Orgs: []config.Org{
+				{
+					ID:   1,
+					Role: "Editor",
+				},
+			},
+		},
+		"bar": {
+			Orgs: []config.Org{
+				{
+					ID:   1,
+					Role: "Admin",
+				},
+			},
+		},
+	}
+
+	expectedMap := userOrgsRoleMap{
+		1: "Admin",
+	}
+
+	// the client is only used to update grafana admin permissions in this case
+	// so it doesn't matter what's the current value of user or orgMap
+	client := NewMockClient(user, userOrgsRoleMap{})
+
+	orgsRoleMap, err := client.UpdateOrgUserAuthz(user, groups)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedMap, orgsRoleMap)
+}
+
+func TestGetOrCreateUser(t *testing.T) {
+
+	// Existing user
+	user := setupUser()
+
+	client := NewMockClient(user, userOrgsRoleMap{})
+
+	orgUser, err := client.GetOrCreateUser("foo")
+	assert.NoError(t, err)
+	assert.Equal(t, user, orgUser)
+
+	// New user
+	newUser, err := client.GetOrCreateUser("new")
+	assert.NoError(t, err)
+	// for convenience the CreateUser mock returns the same ID as the user.ID
+	// passed in NewMockClient
+	assert.Equal(t, int64(1), newUser.ID)
 }
 
 func TestIsRoleAssignable(t *testing.T) {
--- pkg/grafana/mock.go
+++ pkg/grafana/mock.go
@@ -10,7 +10,7 @@ import (
 // MockGAPIClient mimicks the behaviour of required gapi calls
 type mockGAPIClient struct {
 	user       gapi.User
-	orgRoleMap map[int64]models.RoleType
+	orgRoleMap userOrgsRoleMap
 }
 
 func (c *mockGAPIClient) UserByEmail(login string) (gapi.User, error) {
@@ -22,19 +22,29 @@ func (c *mockGAPIClient) UserByEmail(login string) (gapi.User, error) {
 }
 
 func (c *mockGAPIClient) CreateUser(user gapi.User) (int64, error) {
-	return user.ID, nil
+	// for new user return the ID of the provided `user` in mockGAPIClient
+	return c.user.ID, nil
 }
 
-func (c *mockGAPIClient) AddOrgUser(OrgID int64, login string, role string) error {
-
-	if _, ok := c.orgRoleMap[OrgID]; ok {
+func (c *mockGAPIClient) AddOrgUser(orgID int64, login string, role string) error {
+	if _, ok := c.orgRoleMap[orgID]; ok {
 		return errors.New(`status: 409, body: "User is already member of this organization"`)
+	}
+
+	// force an error when orgID is 0
+	// Grafana starts orgIDs from 1 so this is a fair assumption
+	if orgID == 0 {
+		return errors.New("orgID does not exists")
 	}
 
 	return nil
 }
 
 func (c *mockGAPIClient) UpdateOrgUser(orgID, userID int64, role string) error {
+	if userID == 0 {
+		return errors.New("user has no id")
+	}
+
 	return nil
 }
 
