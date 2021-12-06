41b4ab7f13d79b3f58acd14d111a42495a9bf19c
Ricardo Hernandez <ricardo.hernandez@mongodb.com>
2021-12-06 16:04:37 +0000

    improve server test coverage

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -15,8 +15,104 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func newTestJWTToken(subject string) string {
+	cl := jwt.Claims{
+		Email:  fmt.Sprintf("%s@example.com", subject),
+		Groups: []string{"foo", "bar"},
+	}
+	cl.Subject = subject
+
+	tokenString, _ := jwt.NewTestJWTWithClaims(cl)
+
+	return tokenString
+}
+
+func TestTokenValidations(t *testing.T) {
+	// the backendServer represents the Grafana server. In this case we are mocking the Grafana api calls
+	// so the backend server is only here to avoid the proxy to timeout
+	backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer backendServer.Close()
+	backendURL, _ := url.Parse(backendServer.URL)
+
+	client := grafana.NewMockClient(gapi.User{Login: "jhon"}, map[int64]models.RoleType{})
+
+	tests := []struct {
+		name       string
+		cookie     *http.Cookie
+		authorized bool
+	}{
+		{
+			name: "valid JWT token and cookie",
+			cookie: &http.Cookie{
+				Name:  "auth_token",
+				Value: newTestJWTToken("jhon"),
+			},
+			authorized: true,
+		},
+		{
+			name: "valid JWT token without sub",
+			cookie: &http.Cookie{
+				Name:  "auth_token",
+				Value: newTestJWTToken(""),
+			},
+		},
+		{
+			name: "invalid JWT token",
+			cookie: &http.Cookie{
+				Name:  "auth_token",
+				Value: "this-is-no-valid-jwt",
+			},
+		},
+		{
+			name: "invalid cookie",
+			cookie: &http.Cookie{
+				Name: "",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		req := httptest.NewRequest("GET", "/", nil)
+		req.Host = "http://grafana.example.com"
+
+		if test.cookie.Name != "" {
+			req.AddCookie(test.cookie)
+		}
+
+		server, err := New(
+			WithGrafanaProxyURL(backendURL),
+			WithCookieName(test.cookie.Name),
+			WithConfigGroups(config.Groups{}),
+			WithGrafanaClient(client),
+			WithGrafanaResponseHeaders(GrafanaResponseHeaders{
+				User: "X-WEBAUTH-USER",
+			}),
+		)
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		server.ServeHTTP(w, req)
+
+		if test.authorized {
+			assert.Equal(t, http.StatusOK, w.Code)
+		} else {
+			assert.Equal(t, http.StatusUnauthorized, w.Code)
+		}
+	}
+}
+
 func TestHandleRoot(t *testing.T) {
 	t.Parallel()
+
+	// the backendServer represents the Grafana server. In this case we are mocking the Grafana api calls
+	// so the backend server is only here to avoid the proxy to timeout
+	backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer backendServer.Close()
+	backendURL, _ := url.Parse(backendServer.URL)
 
 	groups := config.Groups{
 		"foo": {
@@ -29,39 +125,11 @@ func TestHandleRoot(t *testing.T) {
 		},
 	}
 
-	// the backendServer represents the Grafana server. In this case we are mocking the Grafana api calls
-	// so the backend server is only here to avoid the proxy to timeout
-	backendServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, client")
-	}))
-	defer backendServer.Close()
-	backendURL, _ := url.Parse(backendServer.URL)
-
 	orgRoleMap := map[int64]models.RoleType{
 		1: models.ROLE_EDITOR,
 	}
 
 	client := grafana.NewMockClient(gapi.User{Login: "jhon"}, orgRoleMap)
-
-	req := httptest.NewRequest("GET", "/", nil)
-	req.Host = "http://grafana.example.com"
-
-	cl := jwt.Claims{
-		Email:  "jhon@example.com",
-		Groups: []string{"foo", "bar"},
-	}
-	cl.Subject = "jhon"
-
-	tokenString, err := jwt.NewTestJWTWithClaims(cl)
-	if err != nil {
-		t.Log(err)
-		t.Fail()
-	}
-
-	req.AddCookie(&http.Cookie{
-		Name:  "auth_token",
-		Value: tokenString,
-	})
 
 	server, err := New(
 		WithGrafanaProxyURL(backendURL),
@@ -75,6 +143,13 @@ func TestHandleRoot(t *testing.T) {
 	assert.NoError(t, err)
 
 	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Host = "http://grafana.example.com"
+	req.AddCookie(&http.Cookie{
+		Name:  "auth_token",
+		Value: newTestJWTToken("jhon"),
+	})
+
 	server.ServeHTTP(w, req)
 	assert.Equal(t, "http://grafana.example.com", req.Header.Get("X-Forwarded-Host"))
 	assert.Equal(t, "jhon", req.Header.Get("X-WEBAUTH-USER"))
