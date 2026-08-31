a16129c6f488505fde41d4d0e6a9c27bf6115b61
Yuzhou Liu <yuzhou.liu@mongodb.com>
2026-08-31 14:21:00 -0400

    KANOPY-1530 Remediate dependency CVEs: migrate to go-jose/v4, bump mapstructure and Go base image

--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -1,9 +1,18 @@
 package jwt
 
 import (
+	jose "github.com/go-jose/go-jose/v4"
+	"github.com/go-jose/go-jose/v4/jwt"
 	log "github.com/sirupsen/logrus"
-	"gopkg.in/square/go-jose.v2/jwt"
 )
+
+var signatureAlgorithms = []jose.SignatureAlgorithm{
+	jose.HS256, jose.HS384, jose.HS512,
+	jose.RS256, jose.RS384, jose.RS512,
+	jose.ES256, jose.ES384, jose.ES512,
+	jose.PS256, jose.PS384, jose.PS512,
+	jose.EdDSA,
+}
 
 // Claims is a wrapper of jwt.Claims with added attributes
 type Claims struct {
@@ -14,7 +23,7 @@ type Claims struct {
 
 // TokenClaims returns Claims from a jwt token in raw base64 format
 func TokenClaims(rawToken string) (*Claims, error) {
-	token, err := jwt.ParseSigned(rawToken)
+	token, err := jwt.ParseSigned(rawToken, signatureAlgorithms)
 	if err != nil {
 		log.Error("Error when parsing the token, ", err)
 		return nil, err
